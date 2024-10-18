5284f8646e165e1cb2aced952f488a999a52dacb
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-10-18 19:20:10 +0900

    Update toolchains

--- flake.nix
+++ flake.nix
@@ -2,16 +2,16 @@
   description = "tree-sitter-uiua dev shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.uiua.url        = "github:uiua-lang/uiua";
+  # inputs.uiua.url        = "github:uiua-lang/uiua";
 
-  outputs = { self, nixpkgs, flake-utils, uiua }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.bashInteractive
-          pkgs.nodejs-18_x
+          pkgs.nodejs_latest
           pkgs.iconv
           pkgs.uiua
           # uiua.packages.${system}.default
